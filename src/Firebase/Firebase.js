265d7ba767220fd281789f55c84525be75e5a752
AneudySQ <80012769+AneudySQ@users.noreply.github.com>
2023-02-19 09:21:27 -0400

    trabajando con firebase

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -39,3 +39,7 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+
+export  async function userExists(){
+    
+}
