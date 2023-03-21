a40f0b26b366523daea03d8ade4e8d6299d3ffa6
AneudySQ <80012769+AneudySQ@users.noreply.github.com>
2023-03-20 22:08:34 -0400

    Buscando

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 import About from './Routes/About';
 import { Contacts } from './Routes/Contacts';
 import Index from './Routes/Index';
